d9641526fca2b50930a50c63d269d18b931fc225
Cael Rowley <caelrowley@gmail.com>
2019-01-17 22:44:00 +0000

    Fixed linting errors and added babel

--- app.js
+++ app.js
@@ -1,68 +1,59 @@
-const RabbitService = {}
-RabbitService.setupRabbit = require('./services/rabbitService/setup-rabbit').setupRabbit
-RabbitService.publishMessages = require('./services/rabbitService/publish-messages').publishMessages
+const RabbitService = {};
+RabbitService.setupRabbit = require('./services/rabbitService/setup-rabbit').setupRabbit;
+RabbitService.publishMessages = require('./services/rabbitService/publish-messages').publishMessages;
 
-const BlockchainService = {}
-BlockchainService.querySmartContract = require('./services/blockchain-service/query-smart-contract').querySmartContract
+const BlockchainService = {};
+BlockchainService.querySmartContract = require('./services/blockchain-service/query-smart-contract').querySmartContract;
 
 const log = require('cf-nodejs-logging-support');
-const request = require('request-promise-native');
-const createUuid = require("./utility/create-uuid");
-const env = require('dotenv').config(); // for local testing
+const uuidv4 = require('uuid/v4');
 
 const credentials = {
-   "smartContractURL": process.env.SMART_CONTRACT_URL
+  smartContractURL: process.env.SMART_CONTRACT_URL,
 };
 
 
-log.setLoggingLevel("info");
+log.setLoggingLevel('info');
 
 // 2.5 Second interval
 const POLL_INTERVAL = 2500;
-var authenticated = false;
 let pollTicCount = 1;
 
-const sMessagingserviceUri = process.env.RABBIT_MQ_LOCAL 
+const sMessagingserviceUri = process.env.RABBIT_MQ_LOCAL;
 
 let channel;
 
-log.logMessage("info", "Project Configuration Succesful");
+log.logMessage('info', 'Project Configuration Succesful');
 
-//equivalent to a game loop
+// equivalent to a game loop
 const poller = async () => {
-   try {
-      if (!channel) {
+  try {
+    if (!channel) {
+      channel = await RabbitService.setupRabbit(sMessagingserviceUri);
+    }
+    console.log(channel);
 
-         channel = await RabbitService.setupRabbit(sMessagingserviceUri);
-      }
-      console.log(channel);
+    const uuid = uuidv4();
 
-      let uuid = createUuid.uuidv4();
-      let messages;
-
-      BlockchainService.querySmartContract(credentials.smartContractURL, pollTicCount)
-         .then((response) => {
-            messages = JSON.parse(response).messages;
-            if (!messages)
-               log.logMessage("info", "No data returned from mock-blockchain-query", { "X-correlation-id": uuid });
-            else {
-               if (messages.length > 0) {
-                  RabbitService.publishMessages(messages, channel, uuid)
-               }
-            }
-         }).catch(function (err) {
-            console.log(err);
-            log.logMessage("error", "Fabric service error", { "X-correlation-id": uuid });
-         });
-
-   } catch (err) {
-      log.logMessage("error", "Poll Tic Error");
-      console.log(err);
-   } finally {
-      log.logMessage("info", 'Poll number' + pollTicCount + ' end');
-      pollTicCount++;
-      setTimeout(poller, POLL_INTERVAL);
-   }
+    BlockchainService.querySmartContract(credentials.smartContractURL, pollTicCount)
+      .then((response) => {
+        const { messages } = JSON.parse(response);
+        if (!messages) log.logMessage('info', 'No data returned from mock-blockchain-query', { 'X-correlation-id': uuid });
+        else if (messages.length > 0) {
+          RabbitService.publishMessages(messages, channel, uuid);
+        }
+      }).catch((err) => {
+        console.log(err);
+        log.logMessage('error', 'Fabric service error', { 'X-correlation-id': uuid });
+      });
+  } catch (err) {
+    log.logMessage('error', 'Poll Tic Error');
+    console.log(err);
+  } finally {
+    log.logMessage('info', `Poll number${pollTicCount} end`);
+    pollTicCount += 1;
+    setTimeout(poller, POLL_INTERVAL);
+  }
 };
 
 setImmediate(poller);
--- services/rabbitService/setup-rabbit.js
+++ services/rabbitService/setup-rabbit.js
@@ -2,36 +2,37 @@ const amqp = require('amqplib');
 const log = require('cf-nodejs-logging-support');
 
 async function setupRabbit(sMessagingserviceUri) {
-   let channel;
+  let channel;
 
-   try {
-      channel = await connectToRabbitMQ(sMessagingserviceUri);
+  try {
+    channel = await connectToRabbitMQ(sMessagingserviceUri);
 
-      channel.assertExchange('NewMessageExchange', 'fanout', { durable: true })
+    channel.assertExchange('NewMessageExchange', 'fanout', { durable: true });
 
-      channel.assertQueue('NewMessageQueue', {
-         durable: true
-      });
+    channel.assertQueue('NewMessageQueue', {
+      durable: true,
+    });
 
-      channel.bindQueue('NewMessageQueue', 'NewMessageExchange', '');
+    channel.bindQueue('NewMessageQueue', 'NewMessageExchange', '');
 
-      log.logMessage("info", "Connection to rabbitMQ Successful");
-   } catch (err) {
-      log.logMessage("error", "Error with connection to rabbitMQ");
-   }
+    log.logMessage('info', 'Connection to rabbitMQ Successful');
+  } catch (err) {
+    log.logMessage('error', 'Error with connection to rabbitMQ');
+  }
 
-   return channel;
+  return channel;
 }
 
 async function connectToRabbitMQ(sMessagingserviceUri) {
-   try {
-      var conn = await amqp.connect(sMessagingserviceUri);
-      var oChannel = await conn.createChannel();
-   } catch (err) {
-      log.logMessage("error", "Error with connection to rabbitMQ");
-   } finally {
-      return oChannel;
-   }
+  let oChannel;
+  try {
+    const conn = await amqp.connect(sMessagingserviceUri);
+    oChannel = await conn.createChannel();
+    return oChannel;
+  } catch (err) {
+    log.logMessage('error', 'Error with connection to rabbitMQ');
+  }
+  return oChannel;
 }
 
-module.exports = { "setupRabbit": setupRabbit }
+module.exports = { setupRabbit };
